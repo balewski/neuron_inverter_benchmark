ef0deeadd7fec332924ed0ad927b19acf21170d0
Zhenying Liu <janel@lr67-1-poplar-1.openstack-dev.graphcore.ai>
2022-03-13 23:32:21 +0000

    use the common config for all the settings

--- poptorch/run-pod16.sh
+++ poptorch/run-pod16.sh
@@ -15,7 +15,6 @@ export TEMP=$TEMP_DIR
 export TMP=$TEMP_DIR
 
 REPLICAS=$1
-DESIGN="$REPLICAS"_replicas
 
 # Please modify this data path on your system.
 # It needs to be either shared by multiple hosts,
@@ -25,10 +24,6 @@ DATA_PATH=/localdata/$USER/neuron-data/
 
 LEARNING_RATE=$2
 GRADIENT_ACCUMULATION=$3
+TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 
-if [[ $REPLICAS -eq 1 ]]
-then
-    DESIGN="$REPLICAS"_replica
-fi
-
-poprun -vv --mpi-global-args='--tag-output --allow-run-as-root ' --mpi-local-args=' -x OPAL_PREFIX -x LD_LIBRARY_PATH -x PATH -x PYTHONPATH -x IPUOF_VIPU_API_TIMEOUT=600 -x POPLAR_LOG_LEVEL=INFO -x POPLAR_RUNTIME_OPTIONS -x POPTORCH_CACHE_DIR -x TEMPDIR -x TMPDIR -x TEMP -x TMP' --ipus-per-replica 1 --numa-aware 1 --num-instances $REPLICAS --num-replicas $REPLICAS ./train_replica.py --design $DESIGN --cellName witness2c --outPath /localdata/$USER/ga"$GA"/r"$REPLICAS"_lr"$LEARNING_RATE" --data-path $DATA_PATH --validation --initLR $LEARNING_RATE --gradientAcc $GRADIENT_ACCUMULATION
+poprun -vv --mpi-global-args='--tag-output --allow-run-as-root ' --mpi-local-args=' -x OPAL_PREFIX -x LD_LIBRARY_PATH -x PATH -x PYTHONPATH -x IPUOF_VIPU_API_TIMEOUT=600 -x POPLAR_LOG_LEVEL=INFO -x POPLAR_RUNTIME_OPTIONS -x POPTORCH_CACHE_DIR -x TEMPDIR -x TMPDIR -x TEMP -x TMP' --ipus-per-replica 1 --numa-aware 1 --num-instances $REPLICAS --num-replicas $REPLICAS ./train_replica.py --design common --cellName witness2c --outPath /localdata/$USER/ga"$GA"/r"$REPLICAS"_lr"$LEARNING_RATE"/"$TIMESTAMP" --data-path $DATA_PATH --validation --initLR $LEARNING_RATE --gradientAcc $GRADIENT_ACCUMULATION
